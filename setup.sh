03c62bafc50d787133847cd1391953a602580f91
Adrian Goldwaser <agold@hotmail.com>
2017-07-22 00:23:16 +1000

    add setup script

---
+++ setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Run with sh -c "$(curl -fsSL https://raw.github.com/AdGold/config-manager/master/setup.sh)"
+
+sudo apt install git python3
+
+cd ~/Downloads
+git clone https://github.com/AdGold/config-manager.git
+cd config-manager
+./config_manager.py config.json
+
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
