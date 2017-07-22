d0bccd2ad55c85cbc29dd631a2736be2a93adc6f
AdGold <AdGold@users.noreply.github.com>
2017-07-22 12:34:28 +1000

    Update setup.sh
    Don't require user input

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 # Run with sh -c "$(curl -fsSL https://raw.github.com/AdGold/config-manager/master/setup.sh)"
 
-sudo apt install git python3
+sudo apt install -y git python3
 
 cd ~/Downloads
 git clone https://github.com/AdGold/config-manager.git
